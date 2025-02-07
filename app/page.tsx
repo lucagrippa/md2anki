5899b2df5f22c46ac3a1b94b12d0bfac754301c7
lucagrippa <lucagrip@yahoo.com>
2025-02-07 13:27:18 -0800

    simplify and fix chat history

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,10 @@ import {
 import { ScrollArea } from "@/components/ui/scroll-area"
 
 import { FileInput } from "@/components/file-input";
-import { FileCard } from "@/components/file-card";
 import { FlashcardCard } from "@/components/flashcard-v2";
-import { ChatInput } from "@/components/chat-input";
 import { ChatInterface } from "@/components/chat-interface";
 import { MarkdownViewer } from "@/components/markdown-viewer";
+import { DownloadButton } from "@/components/download-button";
 
 export default function GenerateDeck() {
     const [file, setFile] = useState<File | null>(null);
@@ -33,41 +32,53 @@ export default function GenerateDeck() {
     const { object, submit, isLoading, stop } = useObject({
         api: "/api/generate",
         schema: flashcardSchemaObject,
+        onFinish: (result) => {
+            setFlashcards(result.object?.flashcards || []);
+            const assistantMessage = {
+                role: "assistant",
+                content: result.object?.response || "",
+                id: crypto.randomUUID()
+            } as Message;
+            setChatHistory(prevChatHistory => [...prevChatHistory, assistantMessage]);
+            // object?.response  = ""
+        },
     });
 
-    const handleSubmit = async (query: string) => {
+    const handleSubmit = async (query: string, submittedFile?: File) => {
         console.log("query", query);
         console.log("chat history", chatHistory);
-        console.log("file", file);
-        setChatHistory([...chatHistory, {
-            role: "user",
-            content: query,
-            id: crypto.randomUUID()
-        }]);
+        console.log("file", submittedFile || file);
 
-        if (file) {
-            const fileContent = await file.text();
-            submit({ file: fileContent, messages: chatHistory });
-        } else {
-            submit({ messages: chatHistory });
+        const fileToUse = submittedFile || file;
+        if (!fileToUse) return;
+
+        // Create new message
+        if (query !== "") {
+            const newMessage = {
+                role: "user",
+                content: query,
+                id: crypto.randomUUID()
+            } as Message;
+
+            // Update chat history
+            setChatHistory(prevChatHistory => [...prevChatHistory, newMessage]);
         }
+
+        const fileName = fileToUse.name;
+        const fileContent = await fileToUse.text();
+        submit({ fileName, fileContent, messages: chatHistory, flashcards });
     };
 
-    if (chatHistory.length === 0) return (
+    if (!file) return (
         <div className="flex flex-col items-center justify-center space-y-4 mt-24 w-full max-w-lg">
             <h1 className="font-medium text-4xl mt-4 cursor-pointer hover:text-primary/75 transition-colors" onClick={() => window.location.reload()}> md2anki</h1>
-            {(!file) ? (
-                <FileInput setFile={setFile} />
-            ) : (
-                <FileCard file={file} setFile={setFile} />
-            )}
-            <ChatInput file={file} onSubmit={handleSubmit} isLoading={isLoading} />
+            <FileInput setFile={setFile} onSubmit={handleSubmit} />
         </div>
     )
 
-    if (file && chatHistory.length > 0) return (
-        <ResizablePanelGroup direction="horizontal" className=" pt-4">
-            <ResizablePanel defaultSize={35} className="pl-4 mb-4">
+    if (file && (isLoading || chatHistory.length > 0)) return (
+        <ResizablePanelGroup direction="horizontal" className="pl-6 pr-3 py-6 space-x-1.5">
+            <ResizablePanel defaultSize={35} className="">
                 <ChatInterface
                     chatHistory={chatHistory}
                     onSubmit={handleSubmit}
@@ -77,24 +88,28 @@ export default function GenerateDeck() {
                 />
             </ResizablePanel>
             <ResizableHandle className="bg-transparent" />
-            <ResizablePanel defaultSize={65} className="pl-4">
+            <ResizablePanel defaultSize={65} className="">
                 <Tabs defaultValue="flashcards" className="flex flex-col flex-grow h-full">
                     <div className="flex items-center justify-between">
                         <TabsList>
                             <TabsTrigger value="flashcards">Flashcards</TabsTrigger>
                             <TabsTrigger value={file.name}>{file.name}</TabsTrigger>
                         </TabsList>
+                        <div className="flex items-center justify-between space-x-2">
+                            <p className="text-sm ">{flashcards.length} flashcards </p>
+                            <DownloadButton isLoading={isLoading} />
+                        </div>
                     </div>
-                    <TabsContent value="flashcards" className="flex-grow overflow-auto mb-4">
+                    <TabsContent value="flashcards" className="flex-grow overflow-auto shadow-sm">
                         <ScrollArea className="w-full">
-                            <div className="grid grid-cols-1 gap-4 w-full pr-4">
+                            <div className="grid grid-cols-1 gap-4 w-full pr-3">
                                 {object?.flashcards?.map((flashcard, index) => (
                                     <FlashcardCard key={index} flashcard={flashcard} />
                                 ))}
                             </div>
                         </ScrollArea>
                     </TabsContent>
-                    <TabsContent value={file.name} className="flex-grow overflow-auto mb-4 border-2 rounded-lg">
+                    <TabsContent value={file.name} className="flex-grow overflow-auto border-2 rounded-lg shadow-sm">
                         <MarkdownViewer file={file} />
                     </TabsContent>
                 </Tabs>
