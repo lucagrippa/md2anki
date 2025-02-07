58c1c17da4d44579b5830255b1debbec28366f41
lucagrippa <lucagrip@yahoo.com>
2025-02-07 13:28:22 -0800

    remove avatars and add chat bubble tail

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -12,22 +12,24 @@ interface ChatMessageProps {
 
 export function ChatMessage({ message, className }: ChatMessageProps) {
     return (
-        <div key={message.id} className={`flex flex-row mb-4 space-x-2 items-center ${className}`}>
-            <Avatar>
-                <AvatarImage src={message.role === 'user' ? 'https://github.com/shadcn.png' : 'https://github.com/vercel.png'} />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+        <div key={message.id} className={`flex flex-row mb-4 ${message.role === 'user' ? 'justify-end' : 'justify-start'
+            } ${className}`}>
             <div
-                className={`inline-block p-2 rounded-xl text-sm ${message.role === 'user'
-                    ? 'bg-blue-500 text-white'
-                    : 'bg-gray-200 text-gray-800'
+                className={`relative inline-block py-2 px-3 rounded-xl text-sm ${message.role === 'user'
+                    ? 'bg-blue-500 text-white before:bg-blue-500'
+                    : 'bg-gray-200 text-gray-700 before:bg-gray-200'
+                    } 
+                    before:absolute before:bottom-0 before:h-5 before:w-5 before:z-0
+                    after:absolute after:bottom-0 after:w-2.5 after:h-5 after:bg-white after:z-[1]
+                    ${message.role === 'user'
+                        ? 'before:-right-2 before:rounded-bl-[15px] after:-right-2.5 after:rounded-bl-[10px]'
+                        : 'before:-left-2 before:rounded-br-[15px] after:-left-2.5 after:rounded-br-[10px]'
                     }`}
             >
                 {Array.isArray(message.content)
                     ? message.content.map(part => 'text' in part ? part.text : '').join('')
                     : message.content}
             </div>
-
         </div>
     )
 }
